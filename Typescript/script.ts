cd9494a5727c643a9445e0a5cc2c19e33bf9c07e
Facundo Spinelli <facuspi@hotmail.com>
2022-10-19 16:02:14 -0300

    usando js y ts

---
+++ Typescript/script.ts
@@ -0,0 +1,18 @@
+interface persona {
+    vnombyap: string;
+    vtitulo: string;
+    vlugar: [string, string];
+}
+
+interface expLaboral {
+    vpuesto: string;
+    vempresa: string;
+    vtiempo: [string, string];
+    vdescripcion: string;
+}
+
+interface estudio {
+    vlugar: string;
+    vtiempo: [string, string];
+    vtitulo: string;
+}
